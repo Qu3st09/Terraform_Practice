811a89dee5ccf0cf6dbf1ea9adf5ae6743f510b9
ASIF IQBAL <96387761+Qu3st09@users.noreply.github.com>
2022-01-17 15:49:37 -0500

    Update First_ec2.tf

--- First_ec2.tf
+++ First_ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
- access_key = "AKIAVDUZX2CUSRSEUEK3"
- secret_key = "4AoqpTwi6Q48R50ireBtMj1bqZaKljolema3Lugq"
+ access_key = "Your access key"
+ secret_key = "Your secret key"
 }
 
 resource "aws_instance" "Myec2" {
